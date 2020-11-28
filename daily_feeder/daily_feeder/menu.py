f78704664f1ff44e8eede31242c8ef3b96e9dfb9
Kevin Dodge <kdodge@alianza.com>
2020-11-27 20:57:30 -0700

    Adding ProgramSettings

--- daily_feeder/daily_feeder/menu.py
+++ daily_feeder/daily_feeder/menu.py
@@ -1,5 +1,6 @@
 class Name:
-    def __init__(self, name):
+    def __init__(self, key, name):
+        self._key = key
         self._name = name
 
     def name(self):
@@ -8,6 +9,7 @@ class Name:
 
 class NameValue(Name):
     _value = 0
+
     def __init__(self, max, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._max = max
@@ -36,58 +38,60 @@ class HourSelector(NameValue):
 
 class MenuItem(Name):
     def __init__(self, key, name, values):
-        super().__init__(name)
+        super().__init__(key=key, name=name)
         self._parent = None
         self._key = key
         self._values = values
 
         for value in values:
-            if type(value) == MenuItem:
-                value._parent = self
+            if isinstance(value, MenuItem):
+                value.parent = self
 
+    @property
     def parent(self):
         return self._parent
 
+    @parent.setter
+    def parent(self, parent):
+        self._parent = parent
+
     def values(self):
         values = self._values
-        if self.parent():
+        if self.parent:
             values = values + [self._parent]
         return values
 
     def value_names(self):
         values = [value.name() for value in self._values]
-        if self.parent():
+        if self.parent:
             values += ['Return']
         return values
 
 
+class ProgramSettings(MenuItem):
+    def __init__(self, *args, **kwargs):
+        values=[
+            SecondSelector(key='stir_seconds', name='Stir Seconds'),
+            SecondSelector(key='dose_seconds', name='Dose Seconds'),
+            MenuItem('dose_freq', 'Dose Frequency', values=[
+                HourSelector(key='hour', name='Hours'),
+                MinuteSelector(key='minutes', name='Minutes'),
+            ]),
+            HourSelector(key='start_hour', name='Start Hour'),
+            HourSelector(key='end_hour', name='End Hour'),
+        ]
+        super().__init__(values=values, *args, **kwargs)
+
+
 MENU = MenuItem('root', 'MAIN', values=[
     MenuItem('run_now', 'Run Now', values=[
-        SecondSelector('Dose Seconds')
+        SecondSelector(key='', name='Dose Seconds')
     ]),
-    MenuItem('pg_1', 'Program 1', values=[
-        SecondSelector('Stir Seconds'),
-        SecondSelector('Dose Seconds'),
-        MenuItem('dose_freq', 'Dose Frequence', values=[
-            HourSelector('Hours'),
-            MinuteSelector('Minutes'),
-        ]),
-        HourSelector('Start Hour'),
-        HourSelector('End Hour'),
-    ]),
-    MenuItem('pg_2', 'Program 2', values=[
-        SecondSelector('Stir Seconds'),
-        SecondSelector('Dose Seconds'),
-        MenuItem('dose_freq', 'Dose Frequence', values=[
-            HourSelector('Hours'),
-            MinuteSelector('Minutes'),
-        ]),
-        HourSelector('Start Hour'),
-        HourSelector('End Hour'),
-    ]),
+    ProgramSettings('pg_1', 'Program 1'),
+    ProgramSettings('pg_1', 'Program 2'),
     MenuItem('', 'Set Time', values=[
-        HourSelector('Hour'),
-        MinuteSelector('Minute'),
+        HourSelector(key='', name='Hour'),
+        MinuteSelector(key='', name='Minute'),
     ]),
 ])
 
